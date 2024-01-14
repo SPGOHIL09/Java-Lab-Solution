c12b61130fea6404a8ad0ef40eee1ede20fb7a6e
SPGOHIL09 <shatrunajay009@gmail.com>
2024-01-14 22:58:57 +0530

    dvvf

--- Lab-4/Lab4_5.java
+++ Lab-4/Lab4_5.java
@@ -7,7 +7,13 @@ public class Lab4_5 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter String:" );
         String str = sc.nextLine();
-        int len = 0;
+        int length = str.length();
+        String s = "";
+        for(int i = length / 2 ; i < length ; i++){
+            s += str.charAt(i);
+        }
+        System.out.println("Second Half of String : "+s);
+        sc.close();
         
     }
     
